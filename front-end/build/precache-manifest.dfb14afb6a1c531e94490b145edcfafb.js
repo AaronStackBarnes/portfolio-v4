f8c77cf4927c50a1443b610aa9a6c62b6d4e783f
AaronStack <aaron.stack.barnes@gmail.com>
2018-12-25 16:30:31 -0500

    bug fix

---
+++ front-end/build/precache-manifest.dfb14afb6a1c531e94490b145edcfafb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "18a802fe38314e890943ed9f88354872",
+    "url": "/static/media/asb.18a802fe.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "00f2d570038b508a070a",
+    "url": "/static/js/main.00f2d570.chunk.js"
+  },
+  {
+    "revision": "64ffe752bf1ec94d08cb",
+    "url": "/static/js/1.64ffe752.chunk.js"
+  },
+  {
+    "revision": "00f2d570038b508a070a",
+    "url": "/static/css/main.af39229e.chunk.css"
+  },
+  {
+    "revision": "26d5feecf5c1f1c64a8ea669830bb34f",
+    "url": "/index.html"
+  }
+];
